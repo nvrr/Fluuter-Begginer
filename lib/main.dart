991320c031cf9b100f44a8aa0dc0680ade7df2df
nvrr <sureshvarma809@gmail.com>
2020-09-10 22:19:29 +0530

    List of Data with Custome Classes

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'quotes.dart';
+
+void main() => runApp(MaterialApp(
+  
+  home: QuoteList(),
+),
+);
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key key}) : super(key: key);
+
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  
+List<Quote> quotes = [
+  Quote(text: "dogs on the way bow bow", author: "gram"),
+  Quote(text: "cats on the way meow meow", author: "nigraham"),
+  Quote(text: "rats on the way cheek cheek", author: "whatson")
+];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Awesome Quotes"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent
+      ),
+
+      body: Column(
+        children: quotes.map((quote) => Text("${quote.text} -${quote.author}")).toList()
+        ),
+    );
+  }
+}
